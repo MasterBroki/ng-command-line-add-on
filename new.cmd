bb4628233ab8c5ea42351499a6e1517a8547be91
Charles <charles.brocchiero@hec.ca>
2018-04-15 13:25:38 -0400

    gitignore

--- new.cmd
+++ new.cmd
@@ -1,16 +1,19 @@
-echo off
+@echo off
+if /I "%1" == "" (
+echo "Component name not defined"
+goto end
+)
 SET nomComponent=%1
 if exist "%nomComponent%\NUL" goto end
+@echo on
 call ng generate component %nomComponent%
-cd "%nomComponent%"
-
-ren "%nomComponent%.component.html" "%nomComponent%.template.html"
-ren "%nomComponent%.component.css" "%nomComponent%.stylesheet.css"
-powershell -Command "(gc %nomComponent%.component.ts) -replace '%nomComponent%.component.html', '%nomComponent%.template.html' | Out-File %nomComponent%.component.ts"
-powershell -Command "(gc %nomComponent%.component.ts) -replace '%nomComponent%.component.css', '%nomComponent%.stylesheet.css' | Out-File %nomComponent%.component.ts"
-powershell -Command "(gc %nomComponent%.component.ts) -replace 'app-%nomComponent%', '%nomComponent%' | Out-File %nomComponent%.component.ts"
-git add "%nomComponent%.component.ts"
-git add "%nomComponent%.template.html"
-git add "%nomComponent%.stylesheet.css"
-cd ../
+@echo off
+ren "%nomComponent%\%nomComponent%.component.html" "%nomComponent%\%nomComponent%.template.html"
+ren "%nomComponent%\%nomComponent%.component.css" "%nomComponent%\%nomComponent%.stylesheet.css"
+powershell -Command "(gc %nomComponent%\%nomComponent%.component.ts) -replace '%nomComponent%.component.html', '%nomComponent%.template.html' | Out-File %nomComponent%\%nomComponent%.component.ts"
+powershell -Command "(gc %nomComponent%\%nomComponent%.component.ts) -replace '%nomComponent%.component.css', '%nomComponent%.stylesheet.css' | Out-File %nomComponent%\%nomComponent%.component.ts"
+powershell -Command "(gc %nomComponent%\%nomComponent%.component.ts) -replace 'app-%nomComponent%', '%nomComponent%' | Out-File %nomComponent%\%nomComponent%.component.ts"
+git add "%nomComponent%\%nomComponent%.component.ts"
+git add "%nomComponent%\%nomComponent%.template.html"
+git add "%nomComponent%\%nomComponent%.stylesheet.css"
 :end
